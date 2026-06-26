07ca45cd6d2f96187552e34cb1747c712b88eb89
Ujjawal Kumar <107112307+affnarayani@users.noreply.github.com>
2026-06-26 15:27:22 +0530

    26062026

--- copy_cookies.py
+++ copy_cookies.py
@@ -11,7 +11,10 @@ DESTINATION_FOLDERS = [
     r"D:\Coding\medium_forge\cookies",
     r"D:\Coding\writer_stack\cookies",
     r"D:\Coding\q_rise\chatgpt_cookies",
-    r"D:\Coding\pin_pilot_ujjawal\cookies"
+    r"D:\Coding\pin_pilot_ujjawal\cookies",
+    r"D:\Coding\link_boost_ujjawal\chatgpt_cookies",
+    r"D:\Coding\link_boost_umang\chatgpt_cookies",
+    r"D:\Coding\clear_llm\cookies_gpt"
 ]
 # ===============================================================
 
