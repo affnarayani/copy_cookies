5a5907283e9105cb1a56c13794acec28c1439184
Ujjawal Kumar <107112307+affnarayani@users.noreply.github.com>
2026-07-18 12:05:41 +0530

    18072026

--- fetch_cookies.py
+++ fetch_cookies.py
@@ -18,7 +18,7 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 # =========================
 load_dotenv()
 
-HEADLESS = False
+HEADLESS = True
 TARGET_URL = "https://chatgpt.com/"
 ATOMIC_MAIL_URL = "https://atomicmail.io/"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
